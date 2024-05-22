64711258a5a24ec3d1056104bb54a1801df39f09
Steven Shen <sysheen@gmail.com>
2024-05-22 10:29:00 +0800

    init

---
+++ nodes/CheckEmpty/CheckEmpty.node.ts
@@ -0,0 +1,39 @@
+import {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+
+export class CheckEmpty implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Check Empty',
+		name: 'CheckEmpty',
+		icon: 'file:empty.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'Check the previous node output is empty',
+		defaults: {
+			name: 'CheckEmpty',
+		},
+		inputs: ['main'],
+		outputs: ['main', 'main'],
+		outputNames: ['empty', 'not empty'],
+		credentials: [
+		],
+		properties: [
+		],
+	};
+
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		if (items.length === 1 && Object.keys(items[0].json).length === 0) {
+			return [items, []];
+		} else {
+			return [[], items];
+		}
+	}
+}
